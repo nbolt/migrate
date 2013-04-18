ba97c2e212db9a31786bb98855ed59bb73bc916f
Chris Bolton <c@chrisbolton.me>
2013-04-18 14:46:38 -0700

    createTable

--- src/Database/Migrate.hs
+++ src/Database/Migrate.hs
@@ -3,8 +3,9 @@ module Database.Migrate where
 data Result = Result Bool String
 
 class Migratable m where
-    migrate  :: Int -> m -> IO ()
-    executeM :: m -> IO Bool
-    checkT   :: Int -> m -> IO Bool
-    insertT  :: Int -> m -> IO Bool
+    migrate     :: Int -> m -> IO ()
+    executeM    :: m -> IO Bool
+    checkT      :: Int -> m -> IO Bool
+    insertT     :: Int -> m -> IO Bool
+    createTable :: m -> IO ()
 
