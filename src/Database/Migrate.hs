a4c9f57003ed48b15a228a23142da86818906a29
Chris Bolton <c@chrisbolton.me>
2013-04-18 08:28:57 -0700

    addMigration executable implemented

--- src/Database/Migrate.hs
+++ src/Database/Migrate.hs
@@ -1,3 +1,25 @@
-module Migrate
-  (
-  ) where
+module Main where
+
+import System.Directory
+import Data.UnixTime
+
+data Result = Result String Bool
+
+class Migratable m where
+    migrate :: Int -> m -> IO ()
+
+main :: IO ()
+main = do
+    now    <- getUnixTime
+    exists <- doesFileExist "migrations.hs"
+    if exists
+      then return ()
+      else createMigrationsFile
+    appendFile "migrations.hs" $ newMigration now
+
+createMigrationsFile :: IO ()
+createMigrationsFile = writeFile "migrations.hs" $ unlines ["main = do", "    newMigration"]
+
+newMigration :: UnixTime -> String
+newMigration time = unlines ["", "newMigration = migrate " ++ (show $ utSeconds time) ++ " $ do", ""]
+
