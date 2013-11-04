b8aed9f3d2037ba51f8c480c083fdfd75b796147
Chris Bolton <c@chrisbolton.me>
2013-11-04 00:13:46 -0800

    minor update

--- src/AddMigration.hs
+++ src/AddMigration.hs
@@ -17,7 +17,7 @@ createMigrationsFile = writeFile "migrations.hs" $
     unlines ["import Data.Yaml", "import qualified Data.HashMap.Lazy as HM", "import qualified Data.Text as T", "import Control.Monad.State", "import Database.Migrate", "import Database.Migrate.Backend", "import Database.Backend", "",
              "main = do", "    params' <- decodeFile \"migrations.yml\" :: IO (Maybe Value)", "    case params' of", "        Nothing     -> putStrLn \"migrations.yml file not found\"", "        Just params -> do",
              "            let", "                Object json = toJSON params", "                Just (String dbName) = HM.lookup (T.pack \"database\") json",
-             "            handle' <- liftIO Migratable", "            runStateT migrations handle", "            return ()", "",
+             "            dbHandle <- liftIO ...", "            runStateT migrations Migratable", "            return ()", "",
              "migrations = do", "    createDB", "    createTable"]
 
 newMigration :: UnixTime -> String
